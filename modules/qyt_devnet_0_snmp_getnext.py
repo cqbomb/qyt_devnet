08b3b03bc31183c9a7d87d0315cf2cdb2412f5af
cq_bomb <collinsctk@gmail.com>
2018-12-14 17:43:07 +0800

    修改注释SNMP GETNEXT

--- modules/qyt_devnet_0_snmp_getnext.py
+++ modules/qyt_devnet_0_snmp_getnext.py
@@ -38,43 +38,59 @@ def snmpv2_getnext(ip, community, oid, port=161):
 
 def get_ifs(ip, type, community):
     if type == "switch" or type == "Router":
+        # 获取接口名字列表
         ifs_name_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.2", port=161)
+        # 获取接口物理带宽列表
         ifs_bw_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.5", port=161)
+        # 获取入向字节数列表
         ifs_in_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.10", port=161)
+        # 获取出向字节数列表
         ifs_out_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.16", port=161)
+        # 产生接口名称的列表
         ifs_name_list = [x[1] for x in ifs_name_list_raw]
-        # print(ifs_name_list)
+        # 产生接口物理带宽的列表
         ifs_bw_list = [x[1] for x in ifs_bw_list_raw]
-        # print(ifs_bw_list)
+        # 产生入向字节数的列表
         ifs_in_list = [x[1] for x in ifs_in_list_raw]
-        # print(ifs_in_list)
+        # 产生出向字节数的列表
         ifs_out_list = [x[1] for x in ifs_out_list_raw]
-        # print(ifs_out_list)
+        # zip把数据压到一起,压之后的结果如下
+        # [('Ethernet0/0', '10000000', '62828158', '54853874'), ('Ethernet0/1', '10000000', '37064054', '874844327'), ('Ethernet0/2', '10000000', '856696891', '56022429')]
         ifs_info_zip = zip(ifs_name_list, ifs_bw_list, ifs_in_list, ifs_out_list)
         ifs_info_list = []
         for if_info in ifs_info_zip:
+            # 我们要排除掉,入和出向完全没有任何数据的接口
             if if_info[2] == '0' and if_info[3] == '0':
                 continue
+            # 把存在数据的接口放入infs_info_list列表
             ifs_info_list.append(if_info)
         return ifs_info_list
     elif type == "ASA":
+        # 获取接口名字列表
         ifs_name_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.2", port=161)
+        # 获取接口物理带宽列表
         ifs_bw_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.5", port=161)
+        # 获取入向字节数列表
         ifs_in_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.10", port=161)
+        # 获取出向字节数列表
         ifs_out_list_raw = snmpv2_getnext(ip, community, "1.3.6.1.2.1.2.2.1.16", port=161)
+        # 对ASA的接口名字进行清洗,只留nameif的名称
+        # 产生接口名称的列表
         ifs_name_list = [x[1].replace("Adaptive Security Appliance '", "").replace("' interface", "") for x in ifs_name_list_raw]
-        # print(ifs_name_list)
+        # 产生接口物理带宽的列表
         ifs_bw_list = [x[1] for x in ifs_bw_list_raw]
-        # print(ifs_bw_list)
+        # 产生入向字节数的列表
         ifs_in_list = [x[1] for x in ifs_in_list_raw]
-        # print(ifs_in_list)
+        # 产生出向字节数的列表
         ifs_out_list = [x[1] for x in ifs_out_list_raw]
-        # print(ifs_out_list)
+        # zip把数据压到一起,压之后的结果如下
         ifs_info_zip = zip(ifs_name_list, ifs_bw_list, ifs_in_list, ifs_out_list)
         ifs_info_list = []
         for if_info in ifs_info_zip:
+            # 我们要排除掉,入和出向完全没有任何数据的接口
             if if_info[2] == '0' and if_info[3] == '0':
                 continue
+            # 把存在数据的接口放入infs_info_list列表
             ifs_info_list.append(if_info)
         return ifs_info_list
 
@@ -85,18 +101,7 @@ if __name__ == "__main__":
     asa_ip = "192.168.1.104"
     nexus_ip = "192.168.1.103"
     community = "qytangro"
-    # # 显示接口信息
-    # print(snmpv2_getnext(device_ip, community, "1.3.6.1.2.1.2.2.1.2", port=161))
-    # for x, y in snmpv2_getnext(device_ip, community, "1.3.6.1.2.1.2.2.1.2", port=161):
-    #     print(x, y)
-    # # 接口速率
-    # print(snmpv2_getnext(device_ip, community, "1.3.6.1.2.1.2.2.1.5", port=161))
-    #
-    # # 进接口字节数
-    # print(snmpv2_getnext(device_ip, community, "1.3.6.1.2.1.2.2.1.10", port=161))
-    #
-    # # 出接口字节数
-    # print(snmpv2_getnext(device_ip, community, "1.3.6.1.2.1.2.2.1.16", port=161))
+
     print(get_ifs(device_ip, 'Router', community))
     print(get_ifs(nexus_ip, 'switch', community))
     print(get_ifs(asa_ip, 'ASA', community))
