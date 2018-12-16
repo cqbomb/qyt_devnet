ee4e6d70e0b23dd1eb9b0f7a2f2448ebecdcb7ad
cq_bomb <collinsctk@gmail.com>
2018-12-16 12:30:11 +0800

    接口速率搞定!

--- views/qyt_monitor_device.py
+++ views/qyt_monitor_device.py
@@ -6,7 +6,7 @@
 # 教主技术进化论拓展你的技术新边疆
 # https://ke.qq.com/course/271956?tuin=24199d8a
 
-from qytdb.models import Devicedb, Device_reachable, Devicecpumem, Deviceinterfaces, Devicestatus
+from qytdb.models import Devicedb, Device_reachable, Devicecpumem, Deviceinterfaces, Devicestatus, Deviceinterfaces_utilization
 from django.shortcuts import render
 from datetime import datetime, timedelta, timezone
 import json
@@ -85,11 +85,83 @@ def monitor_mem_dev(request, devicename):
 
 
 def monitor_if_speed(request):
-    return render(request, 'monitor_devices_if_speed.html')
+    result = Devicedb.objects.all()
+    devices_list = []
+    for x in result:
+        devices_list.append(x.name)
+
+    if_speed = Deviceinterfaces_utilization.objects.filter(name=devices_list[0], date__gte=datetime.now() - timedelta(hours=1))
+    ifs_name = Deviceinterfaces.objects.get(name=devices_list[0])
+
+    if_speed_rx_data = []
+    if_speed_tx_data = []
+    if_speed_time = []
+
+    tzutc_8 = timezone(timedelta(hours=8))
+    for x in if_speed:
+        if_speed_rx_data.append(json.loads(x.interfaces_current_speed_rx))
+        if_speed_tx_data.append(json.loads(x.interfaces_current_speed_tx))
+        if_speed_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+
+    speed_rx_data_list = []
+    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+        name_speed_rx_list = []
+        for x in if_speed_rx_data:
+            for y in x:
+                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                    name_speed_rx_list.append(round(y[1]/1000, 2))  # 控制浮点数的境地
+        speed_rx_data_list.append([name, name_speed_rx_list, if_speed_time])
+
+    speed_tx_data_list = []
+    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+        name_speed_tx_list = []
+        for x in if_speed_tx_data:
+            for y in x:
+                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                    name_speed_tx_list.append(round(y[1]/1000, 2))  # 控制浮点数的境地
+        speed_tx_data_list.append([name, name_speed_tx_list, if_speed_time])
+
+    return render(request, 'monitor_devices_if_speed.html', {'devices_list': devices_list, 'current': devices_list[0], 'if_list': json.loads(ifs_name.interfaces), 'speed_rx_data_list': json.dumps(speed_rx_data_list), 'speed_tx_data_list': json.dumps(speed_tx_data_list)})
 
 
-def monitor_if_speed_dev(request):
-    return render(request, 'monitor_devices_if_speed.html')
+def monitor_if_speed_dev(request, devicename):
+    result = Devicedb.objects.all()
+    devices_list = []
+    for x in result:
+        devices_list.append(x.name)
+
+    if_speed = Deviceinterfaces_utilization.objects.filter(name=devicename, date__gte=datetime.now() - timedelta(hours=1))
+    ifs_name = Deviceinterfaces.objects.get(name=devicename)
+
+    if_speed_rx_data = []
+    if_speed_tx_data = []
+    if_speed_time = []
+
+    tzutc_8 = timezone(timedelta(hours=8))
+    for x in if_speed:
+        if_speed_rx_data.append(json.loads(x.interfaces_current_speed_rx))
+        if_speed_tx_data.append(json.loads(x.interfaces_current_speed_tx))
+        if_speed_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+
+    speed_rx_data_list = []
+    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+        name_speed_rx_list = []
+        for x in if_speed_rx_data:
+            for y in x:
+                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                    name_speed_rx_list.append(round(y[1]/1000, 2))  # 控制浮点数的境地
+        speed_rx_data_list.append([name, name_speed_rx_list, if_speed_time])
+
+    speed_tx_data_list = []
+    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+        name_speed_tx_list = []
+        for x in if_speed_tx_data:
+            for y in x:
+                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                    name_speed_tx_list.append(round(y[1]/1000, 2))  # 控制浮点数的境地
+        speed_tx_data_list.append([name, name_speed_tx_list, if_speed_time])
+
+    return render(request, 'monitor_devices_if_speed.html', {'devices_list': devices_list, 'current': devicename, 'if_list': json.loads(ifs_name.interfaces), 'speed_rx_data_list': json.dumps(speed_rx_data_list), 'speed_tx_data_list': json.dumps(speed_tx_data_list)})
 
 
 def monitor_if_utilization(request):
