9aa03af7fd4cf93118ec90eca25a6e548d2f7c90
cq_bomb <collinsctk@gmail.com>
2018-12-20 15:13:21 +0800

    sleep2

--- views/qyt_monitor_device.py
+++ views/qyt_monitor_device.py
@@ -19,44 +19,48 @@ def get_device_if_speed_info(devicename="default"):
     devices_list = []
     for x in result:
         devices_list.append(x.name)
+    try:
+        if devicename == 'default':
+            devicename = devices_list[0]
 
-    if devicename == 'default':
-        devicename = devices_list[0]
 
-    if_speed = Deviceinterfaces_utilization.objects.filter(name=devicename,
-                                                           date__gte=datetime.now() - timedelta(hours=1))
-    ifs_name = Deviceinterfaces.objects.get(name=devicename)
+        if_speed = Deviceinterfaces_utilization.objects.filter(name=devicename,
+                                                               date__gte=datetime.now() - timedelta(hours=1))
 
-    if_speed_rx_data = []
-    if_speed_tx_data = []
-    if_speed_time = []
+        ifs_name = Deviceinterfaces.objects.get(name=devicename)
 
-    tzutc_8 = timezone(timedelta(hours=8))
-    for x in if_speed:
-        try:
-            if_speed_rx_data.append(json.loads(x.interfaces_current_speed_rx))
-            if_speed_tx_data.append(json.loads(x.interfaces_current_speed_tx))
-            if_speed_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
-        except Exception:
-            continue
-    speed_rx_data_list = []
-    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
-        name_speed_rx_list = []
-        for x in if_speed_rx_data:
-            for y in x:
-                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
-                    name_speed_rx_list.append(y[1])
-        speed_rx_data_list.append([name, name_speed_rx_list, if_speed_time])
+        if_speed_rx_data = []
+        if_speed_tx_data = []
+        if_speed_time = []
 
-    speed_tx_data_list = []
-    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
-        name_speed_tx_list = []
-        for x in if_speed_tx_data:
-            for y in x:
-                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
-                    name_speed_tx_list.append(y[1])
-        speed_tx_data_list.append([name, name_speed_tx_list, if_speed_time])
-    return devices_list, json.loads(ifs_name.interfaces), speed_rx_data_list, speed_tx_data_list
+        tzutc_8 = timezone(timedelta(hours=8))
+        for x in if_speed:
+            try:
+                if_speed_rx_data.append(json.loads(x.interfaces_current_speed_rx))
+                if_speed_tx_data.append(json.loads(x.interfaces_current_speed_tx))
+                if_speed_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+            except Exception:
+                continue
+        speed_rx_data_list = []
+        for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+            name_speed_rx_list = []
+            for x in if_speed_rx_data:
+                for y in x:
+                    if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                        name_speed_rx_list.append(y[1])
+            speed_rx_data_list.append([name, name_speed_rx_list, if_speed_time])
+
+        speed_tx_data_list = []
+        for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+            name_speed_tx_list = []
+            for x in if_speed_tx_data:
+                for y in x:
+                    if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                        name_speed_tx_list.append(y[1])
+            speed_tx_data_list.append([name, name_speed_tx_list, if_speed_time])
+        return devices_list, json.loads(ifs_name.interfaces), speed_rx_data_list, speed_tx_data_list
+    except Exception:
+        return None
 
 
 def get_device_if_utilization_info(devicename="default"):
@@ -64,45 +68,47 @@ def get_device_if_utilization_info(devicename="default"):
     devices_list = []
     for x in result:
         devices_list.append(x.name)
+    try:
+        if devicename == 'default':
+            devicename = devices_list[0]
 
-    if devicename == 'default':
-        devicename = devices_list[0]
+        if_utilization = Deviceinterfaces_utilization.objects.filter(name=devicename,
+                                                               date__gte=datetime.now() - timedelta(hours=1))
+        ifs_name = Deviceinterfaces.objects.get(name=devicename)
 
-    if_utilization = Deviceinterfaces_utilization.objects.filter(name=devicename,
-                                                           date__gte=datetime.now() - timedelta(hours=1))
-    ifs_name = Deviceinterfaces.objects.get(name=devicename)
+        if_utilization_rx_data = []
+        if_utilization_tx_data = []
+        if_utilization_time = []
 
-    if_utilization_rx_data = []
-    if_utilization_tx_data = []
-    if_utilization_time = []
+        tzutc_8 = timezone(timedelta(hours=8))
+        for x in if_utilization:
+            try:
+                if_utilization_rx_data.append(json.loads(x.interfaces_current_utilization_rx))
+                if_utilization_tx_data.append(json.loads(x.interfaces_current_utilization_tx))
+                if_utilization_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+            except Exception:
+                continue
 
-    tzutc_8 = timezone(timedelta(hours=8))
-    for x in if_utilization:
-        try:
-            if_utilization_rx_data.append(json.loads(x.interfaces_current_utilization_rx))
-            if_utilization_tx_data.append(json.loads(x.interfaces_current_utilization_tx))
-            if_utilization_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
-        except Exception:
-            continue
+        utilization_rx_data_list = []
+        for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+            name_utilization_rx_list = []
+            for x in if_utilization_rx_data:
+                for y in x:
+                    if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                        name_utilization_rx_list.append(y[1])
+            utilization_rx_data_list.append([name, name_utilization_rx_list, if_utilization_time])
 
-    utilization_rx_data_list = []
-    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
-        name_utilization_rx_list = []
-        for x in if_utilization_rx_data:
-            for y in x:
-                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
-                    name_utilization_rx_list.append(y[1])
-        utilization_rx_data_list.append([name, name_utilization_rx_list, if_utilization_time])
-
-    utilization_tx_data_list = []
-    for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
-        name_utilization_tx_list = []
-        for x in if_utilization_tx_data:
-            for y in x:
-                if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
-                    name_utilization_tx_list.append(y[1])
-        utilization_tx_data_list.append([name, name_utilization_tx_list, if_utilization_time])
-    return devices_list, json.loads(ifs_name.interfaces), utilization_rx_data_list, utilization_tx_data_list
+        utilization_tx_data_list = []
+        for name in json.loads(ifs_name.interfaces):  # 循环得到每一个接口名字
+            name_utilization_tx_list = []
+            for x in if_utilization_tx_data:
+                for y in x:
+                    if y[0] == name:  # 找到匹配接口的数据,并把它放入清单
+                        name_utilization_tx_list.append(y[1])
+            utilization_tx_data_list.append([name, name_utilization_tx_list, if_utilization_time])
+        return devices_list, json.loads(ifs_name.interfaces), utilization_rx_data_list, utilization_tx_data_list
+    except Exception:
+        return None
 
 
 def monitor_cpu(request):
@@ -111,16 +117,23 @@ def monitor_cpu(request):
     for x in result:
         devices_list.append(x.name)
 
-    cpus = Devicestatus.objects.filter(name=devices_list[0], date__gte=datetime.now() - timedelta(hours=1))
+    try:
+        devicename = devices_list[0]
 
-    cpu_data = []
-    cpu_time = []
-    tzutc_8 = timezone(timedelta(hours=8))
-    for x in cpus:
-        cpu_data.append(x.cpu)
-        cpu_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
 
-    return render(request, 'monitor_devices_cpu.html', {'devices_list': devices_list, 'current': devices_list[0], 'cpu_data': json.dumps(cpu_data), 'cpu_time': json.dumps(cpu_time)})
+        cpus = Devicestatus.objects.filter(name=devicename, date__gte=datetime.now() - timedelta(hours=1))
+
+        cpu_data = []
+        cpu_time = []
+        tzutc_8 = timezone(timedelta(hours=8))
+        for x in cpus:
+            cpu_data.append(x.cpu)
+            cpu_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+
+        return render(request, 'monitor_devices_cpu.html', {'devices_list': devices_list, 'current': devicename, 'cpu_data': json.dumps(cpu_data), 'cpu_time': json.dumps(cpu_time)})
+
+    except Exception:
+        return render(request, 'monitor_devices_cpu.html')
 
 
 def monitor_cpu_dev(request, devicename):
@@ -147,16 +160,21 @@ def monitor_mem(request):
     for x in result:
         devices_list.append(x.name)
 
-    mems = Devicestatus.objects.filter(name=devices_list[0], date__gte=datetime.now() - timedelta(hours=1))
+    try:
+        devicename = devices_list[0]
 
-    mem_data = []
-    mem_time = []
-    tzutc_8 = timezone(timedelta(hours=8))
-    for x in mems:
-        mem_data.append(x.mem)
-        mem_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+        mems = Devicestatus.objects.filter(name=devicename, date__gte=datetime.now() - timedelta(hours=1))
 
-    return render(request, 'monitor_devices_mem.html', {'devices_list': devices_list, 'current': devices_list[0], 'mem_data': json.dumps(mem_data), 'mem_time': json.dumps(mem_time)})
+        mem_data = []
+        mem_time = []
+        tzutc_8 = timezone(timedelta(hours=8))
+        for x in mems:
+            mem_data.append(x.mem)
+            mem_time.append(x.date.astimezone(tzutc_8).strftime('%H:%M'))
+
+        return render(request, 'monitor_devices_mem.html', {'devices_list': devices_list, 'current': devicename, 'mem_data': json.dumps(mem_data), 'mem_time': json.dumps(mem_time)})
+    except Exception:
+        return render(request, 'monitor_devices_mem.html')
 
 
 def monitor_mem_dev(request, devicename):
@@ -180,10 +198,13 @@ def monitor_mem_dev(request, devicename):
 def monitor_if_speed(request):
     if_data = get_device_if_speed_info("default")
     # 解决Ethernet0/0名称问题
-    if_list = if_data[1].copy()
-    if_list_translate = [x.replace('/', '-') for x in if_list]
+    if if_data:
+        if_list = if_data[1].copy()
+        if_list_translate = [x.replace('/', '-') for x in if_list]
 
-    return render(request, 'monitor_devices_if_speed.html', {'devices_list': if_data[0], 'current': if_data[0][0], 'if_list': if_list_translate, 'if_name': if_data[2][0][0],  'speed_data_list': if_data[2][0][1],   'speed_time_list': if_data[2][0][2]})
+        return render(request, 'monitor_devices_if_speed.html', {'devices_list': if_data[0], 'current': if_data[0][0], 'if_list': if_list_translate, 'if_name': if_data[2][0][0],  'speed_data_list': if_data[2][0][1],   'speed_time_list': if_data[2][0][2]})
+    else:
+        return render(request, 'monitor_devices_if_speed.html')
 
 
 def monitor_if_speed_dev(request, devicename):
@@ -210,13 +231,16 @@ def monitor_if_speed_dev_if_direc(request, devicename, ifname, direction):
 def monitor_if_utilization(request):
     if_data = get_device_if_utilization_info("default")
     # 解决Ethernet0/0名称问题
-    if_list = if_data[1].copy()
-    if_list_translate = [x.replace('/', '-') for x in if_list]
+    if if_data:
+        if_list = if_data[1].copy()
+        if_list_translate = [x.replace('/', '-') for x in if_list]
 
-    return render(request, 'monitor_devices_if_utilization.html',
-                  {'devices_list': if_data[0], 'current': if_data[0][0], 'if_list': if_list_translate,
-                   'if_name': if_data[2][0][0], 'utilization_data_list': if_data[2][0][1],
-                   'utilization_time_list': if_data[2][0][2]})
+        return render(request, 'monitor_devices_if_utilization.html',
+                      {'devices_list': if_data[0], 'current': if_data[0][0], 'if_list': if_list_translate,
+                       'if_name': if_data[2][0][0], 'utilization_data_list': if_data[2][0][1],
+                       'utilization_time_list': if_data[2][0][2]})
+    else:
+        return render(request, 'monitor_devices_if_utilization.html')
 
 
 def monitor_if_utilization_dev(request, devicename):
