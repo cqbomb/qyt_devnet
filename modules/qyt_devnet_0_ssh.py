778e74020069aee1d2d0976398f7443c22834ffe
cq_bomb <collinsctk@gmail.com>
2018-12-20 09:49:48 +0800

    获取配置和HASH！写入数据库搞定！

--- modules/qyt_devnet_0_ssh.py
+++ modules/qyt_devnet_0_ssh.py
@@ -37,6 +37,31 @@ def ssh_multicmd(ip, username, password, cmd_list, verbose=True):
     ssh.close()  # 退出ssh会话
 
 
+def ssh_multicmd_asa(ip, username, password, cmd_list, verbose=True):
+    ssh = paramiko.SSHClient()  # 创建SSH Client
+    ssh.load_system_host_keys()  # 加载系统SSH密钥
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 添加新的SSH密钥
+    ssh.connect(ip, port=22, username=username, password=password, timeout=5, compress=True)  # SSH连接
+
+    chan = ssh.invoke_shell()  # 激活交互式shell
+    time.sleep(1)
+    x = chan.recv(2048).decode()  # 接收回显信息
+    cmd_list_len = len(cmd_list)
+    i = 1
+    for cmd in cmd_list:  # 读取命令
+        chan.send(cmd.encode())  # 执行命令，注意字串都需要编码为二进制字串
+        chan.send(b'\n')  # 一定要注意输入回车
+        time.sleep(2)  # 由于有些回显可能过长，所以可以考虑等待更长一些时间
+        x = chan.recv(40960).decode()  # 读取回显，有些回想可能过长，请把接收缓存调大
+        # 此处对源码进行了调整,只是显示最后一个命令的回显
+        if verbose and i == cmd_list_len:
+            return x  # 打印回显
+        i += 1
+
+    chan.close()  # 退出交互式shell
+    ssh.close()  # 退出ssh会话
+
+
 def ssh_singlecmd(ip, username, password, cmd):
     try:
         ssh = paramiko.SSHClient()  # 创建SSH Client
@@ -81,7 +106,8 @@ def ssh_sure_shell_login(ip, type, username, password, enable_password="Cisc0123
 
 if __name__ == '__main__':
     # 使用Linux解释器 & WIN解释器
-    print(ssh_sure_shell_login('192.168.1.101', 'switch', 'admin', 'Cisc0123'))
-    print(ssh_sure_shell_login('192.168.1.104', 'ASA', 'admin', 'Cisc0123', 'Cisc0123'))
-    print(ssh_sure_shell_login('192.168.1.105', 'Router', 'admin', 'Cisc0123'))
+    # print(ssh_sure_shell_login('192.168.1.101', 'switch', 'admin', 'Cisc0123'))
+    # print(ssh_sure_shell_login('192.168.1.104', 'ASA', 'admin', 'Cisc0123', 'Cisc0123'))
+    # print(ssh_sure_shell_login('192.168.1.105', 'Router', 'admin', 'Cisc0123'))
+    print(ssh_multicmd_asa('192.168.1.104', 'admin', 'Cisc0123', ['enable', 'Cisc0123', 'terminal pager 0', 'show run']))
 
---
+++ modules/qyt_devnet_5_get_config_md5.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚由亁颐堂现任明教教主编写，用于乾颐盾Python课程！
+# 教主QQ:605658506
+# 亁颐堂官网www.qytang.com
+# 教主技术进化论拓展你的技术新边疆
+# https://ke.qq.com/course/271956?tuin=24199d8a
+
+from qyt_devnet_0_ssh import ssh_singlecmd, ssh_multicmd, ssh_multicmd_asa
+import re
+import pg8000
+import hashlib
+from datetime import datetime
+
+psql_ip = "192.168.1.11"
+psql_username = "qytangdbuser"
+psql_password = "Cisc0123"
+psql_db_name = "qytangdb"
+
+
+def get_md5_config():
+    # 连接数据库
+    conn = pg8000.connect(host=psql_ip, user=psql_username, password=psql_password, database=psql_db_name)
+    cursor = conn.cursor()
+    # 查询数据库qytdb_devicedb,获取ip, type, name, snmp_ro_community, ssh_username, ssh_password, enable_password等信息
+    cursor.execute("select ip, type, name, snmp_ro_community, ssh_username, ssh_password, enable_password from qytdb_devicedb")
+    result = cursor.fetchall()
+    for device in result:
+        if device[1] == 'Router':
+            run_config_raw = ssh_singlecmd(str(device[0]), device[4], device[5], 'show run')
+            list_run_config = run_config_raw.split('\n')
+            location = 0
+            host_location = 0
+            for i in list_run_config:
+                if re.match('.*hostname .*', i):
+                    host_location = location  # 定位hostname所在位置
+                else:
+                    location += 1
+            list_run_config = list_run_config[host_location:]  # 截取hostname开始往后的部分
+            run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
+
+            m = hashlib.md5()
+            m.update(run_config.encode())
+            md5_value = m.hexdigest()
+
+            sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
+            cursor.execute(sqlcmd)
+            conn.commit()
+
+        elif device[1] == 'switch':
+            run_config_raw = ssh_singlecmd(str(device[0]), device[4], device[5], 'show run')
+            list_run_config = run_config_raw.split('\n')
+            location = 0
+            host_location = 0
+            for i in list_run_config:
+                if re.match('.*hostname .*', i):
+                    host_location = location  # 定位hostname所在位置
+                else:
+                    location += 1
+            list_run_config = list_run_config[host_location:]  # 截取hostname开始往后的部分
+            run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
+
+            m = hashlib.md5()
+            m.update(run_config.encode())
+            md5_value = m.hexdigest()
+
+            sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
+            cursor.execute(sqlcmd)
+            conn.commit()
+        elif device[1] == 'ASA':
+            run_config_raw = ssh_multicmd_asa(str(device[0]), device[4], device[5], ['enable', device[6], 'terminal pager 0', 'more system:running-config'])
+            list_run_config = run_config_raw.split('\n')
+            location = 0
+            host_location = 0
+            for i in list_run_config:
+                if re.match('^hostname .*', i):
+                    host_location = location  # 定位hostname所在位置
+                else:
+                    location += 1
+            list_run_config = list_run_config[host_location:-4]  # 截取hostname开始往后的部分
+            run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
+
+            m = hashlib.md5()
+            m.update(run_config.encode())
+            md5_value = m.hexdigest()
+
+            sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
+            cursor.execute(sqlcmd)
+            conn.commit()
+
+
+if __name__ == '__main__':
+    get_md5_config()
