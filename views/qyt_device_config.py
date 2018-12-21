ab4f774c4233f5cb204075271e341fb0eb58b80c
cq_bomb <collinsctk@gmail.com>
2018-12-21 09:23:35 +0800

    删除配置操作搞定！

--- views/qyt_device_config.py
+++ views/qyt_device_config.py
@@ -9,6 +9,7 @@
 from qytdb.models import Deviceconfig, Devicedb
 from django.shortcuts import render
 from datetime import datetime, timedelta, timezone
+from django.http import HttpResponseRedirect
 
 
 def device_config(request):
@@ -29,9 +30,9 @@ def device_config(request):
             device_config_date_hash.append({'name': devicename,
                                             'hash': x.hash,
                                             'date': x.date.astimezone(tzutc_8).strftime('%Y-%m-%d %H:%M'),
-                                            'delete_url': '/deviceconfig/delete/' + devicename + '/' + str(x.id) + '/',
-                                            'show_url': '/deviceconfig/show/' + devicename + '/' + str(x.id) + '/',
-                                            'download_url': '/deviceconfig/download/' + devicename + '/' + str(+ x.id) + '/'})
+                                            'delete_url': '/deviceconfig/delete/' + devicename + '/' + str(x.id),
+                                            'show_url': '/deviceconfig/show/' + devicename + '/' + str(x.id),
+                                            'download_url': '/deviceconfig/download/' + devicename + '/' + str(+ x.id)})
         # print(device_config_date_hash)
         return render(request, 'device_config.html', {'devices_list': devices_list, 'current': devicename, 'device_config_date_hash': device_config_date_hash})
 
@@ -59,9 +60,9 @@ def device_config_dev(request, devname):
                 {'name': devicename,
                  'hash': x.hash,
                  'date': x.date.astimezone(tzutc_8).strftime('%Y-%m-%d %H:%M'),
-                 'delete_url': '/deviceconfig/delete/' + devicename + '/' + str(x.id) + '/',
-                 'show_url': '/deviceconfig/show/' + devicename + '/' + str(x.id) + '/',
-                 'download_url': '/deviceconfig/download/' + devicename + '/' + str(+ x.id) + '/'})
+                 'delete_url': '/deviceconfig/delete/' + devicename + '/' + str(x.id),
+                 'show_url': '/deviceconfig/show/' + devicename + '/' + str(x.id),
+                 'download_url': '/deviceconfig/download/' + devicename + '/' + str(+ x.id)})
         # print(device_config_date_hash)
         return render(request, 'device_config.html', {'devices_list': devices_list, 'current': devicename,
                                                       'device_config_date_hash': device_config_date_hash})
@@ -76,7 +77,11 @@ def device_show_config(request, devname, id):
 
 
 def device_del_config(request, devname, id):
-    pass
+    m = Deviceconfig.objects.filter(name=devname, id=id)
+    for x in m:
+        x.delete()
+
+    return HttpResponseRedirect('/deviceconfig/' + devname)
 
 
 def device_download_config(request, devname, id):
