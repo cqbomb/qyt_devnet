7173ef845682b6df10c38e269379a33873f1521a
cq_bomb <collinsctk@gmail.com>
2018-12-24 10:42:48 +0800

    ELK集成!

--- qyt_devnet/urls.py
+++ qyt_devnet/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from views import index, top, qyt_login, qyt_add_devices, qyt_show_devices, qyt_del_device, qyt_edit_device
-from views import qyt_monitor_device, qyt_home, qyt_device_config, qyt_netflow, qyt_sysconfig
+from views import qyt_monitor_device, qyt_home, qyt_device_config, qyt_netflow, qyt_sysconfig, qyt_log
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -62,4 +62,5 @@ urlpatterns = [
     path('sysconfig/reset_threshold_mail', qyt_sysconfig.sysconfig_reset_threshold_mail),
     path('sysconfig/reset_netflow', qyt_sysconfig.sysconfig_reset_netflow),
     path('sysconfig/reset_netflow_db', qyt_sysconfig.sysconfig_reset_netflow_db),
+    path('log/elk', qyt_log.log_elk),
 ]
