e14c61d9071e4a8d84a7792e7afc73490d8d4853
cq_bomb <collinsctk@gmail.com>
2018-12-25 18:03:02 +0800

    注释!

--- views/qyt_del_device.py
+++ views/qyt_del_device.py
@@ -18,27 +18,29 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def del_device(request, devicename):
-
+    # 删除Devicedb表中记录的设备信息
     m = Devicedb.objects.get(name=devicename)
     m.delete()
+    # 删除Device_reachable表中记录的可达性信息
     m = Device_reachable.objects.get(name=devicename)
     m.delete()
+    # 删除Devicecpumem表中记录的cpu和内存利用率的信息
     m = Devicecpumem.objects.get(name=devicename)
     m.delete()
+    # 删除Deviceinterfaces表中记录的接口信息
     m = Deviceinterfaces.objects.get(name=devicename)
     m.delete()
+    # 删除Deviceinterfaces_utilization表中记录的接口利用率条目,由于条目随着时间增加而增加,所以需要使用filter + for循环
     m = Deviceinterfaces_utilization.objects.filter(name=devicename)
     for x in m:
         x.delete()
+    # 删除Devicestatus表中记录的设备状态条目,由于条目随着时间增加而增加,所以需要使用filter + for循环
     m = Devicestatus.objects.filter(name=devicename)
     for x in m:
         x.delete()
+    # 删除Deviceconfig表中备份的设备配置条目,由于条目随着时间增加而增加,所以需要使用filter + for循环
     m = Deviceconfig.objects.filter(name=devicename)
     for x in m:
         x.delete()
-
+    # 删除所有与设备相关条目后,重定向返回显示设备信息页面
     return HttpResponseRedirect('/showdevices/')
-
-
-if __name__ == '__main__':
-    pass
