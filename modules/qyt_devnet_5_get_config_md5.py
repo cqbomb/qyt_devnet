113278207d3640d2c7f1969df6263b73926a34be
cq_bomb <collinsctk@gmail.com>
2018-12-25 11:06:41 +0800

    注释!

--- modules/qyt_devnet_5_get_config_md5.py
+++ modules/qyt_devnet_5_get_config_md5.py
@@ -11,11 +11,7 @@ import re
 import pg8000
 import hashlib
 from datetime import datetime
-
-psql_ip = "192.168.1.11"
-psql_username = "qytangdbuser"
-psql_password = "Cisc0123"
-psql_db_name = "qytangdb"
+from qyt_devnet_0_DB_login import psql_ip, psql_username, psql_password, psql_db_name
 
 
 def get_md5_config():
@@ -26,11 +22,12 @@ def get_md5_config():
     cursor.execute("select ip, type, name, snmp_ro_community, ssh_username, ssh_password, enable_password from qytdb_devicedb")
     result = cursor.fetchall()
     for device in result:
-        if device[1] == 'Router':
+        if device[1] == 'Router':  # 如果设备是路由器
+            # 获取设备show run
             run_config_raw = ssh_singlecmd(str(device[0]), device[4], device[5], 'show run')
             list_run_config = run_config_raw.split('\n')
             location = 0
-            host_location = 0
+            host_location = 0  # 用来找到hostname出现的位置
             for i in list_run_config:
                 if re.match('.*hostname .*', i):
                     host_location = location  # 定位hostname所在位置
@@ -39,24 +36,28 @@ def get_md5_config():
             list_run_config = list_run_config[host_location:]  # 截取hostname开始往后的部分
             run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
 
+            # 计算获取配置的MD5值
             m = hashlib.md5()
             m.update(run_config.encode())
             md5_value = m.hexdigest()
+            # 获取最近一次保存配置的MD5值,注意添加设备的时候就会做一次备份!
+            # order by date desc limit 1 查询最近一次记录
             cursor.execute("select hash from qytdb_deviceconfig where name = '" + device[2] + "' order by date desc limit 1")
             result = cursor.fetchall()
-            if result[0][0] == md5_value:
+            if result[0][0] == md5_value:  # 如果本次配置的MD5值,与上一次备份配置的MD5值相同!略过此次操作
                 continue
             else:
-                # print('write db')
+                # 如果本次配置的MD5值,与上一次备份配置的MD5值不相同,备份配置与MD5值到数据库
                 sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
                 cursor.execute(sqlcmd)
                 conn.commit()
 
-        elif device[1] == 'switch':
+        elif device[1] == 'switch':  # 如果设备是交换机
+            # 获取设备show run
             run_config_raw = ssh_singlecmd(str(device[0]), device[4], device[5], 'show run')
             list_run_config = run_config_raw.split('\n')
             location = 0
-            host_location = 0
+            host_location = 0  # 用来找到hostname出现的位置
             for i in list_run_config:
                 if re.match('.*hostname .*', i):
                     host_location = location  # 定位hostname所在位置
@@ -65,42 +66,50 @@ def get_md5_config():
             list_run_config = list_run_config[host_location:]  # 截取hostname开始往后的部分
             run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
 
+            # 计算获取配置的MD5值
             m = hashlib.md5()
             m.update(run_config.encode())
             md5_value = m.hexdigest()
 
+            # 获取最近一次保存配置的MD5值,注意添加设备的时候就会做一次备份!
+            # order by date desc limit 1 查询最近一次记录
             cursor.execute("select hash from qytdb_deviceconfig where name = '" + device[2] + "' order by date desc limit 1")
             result = cursor.fetchall()
-            if result[0][0] == md5_value:
+            if result[0][0] == md5_value:  # 如果本次配置的MD5值,与上一次备份配置的MD5值相同!略过此次操作
                 continue
             else:
-                # print('write db')
+                # 如果本次配置的MD5值,与上一次备份配置的MD5值不相同,备份配置与MD5值到数据库
                 sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
                 cursor.execute(sqlcmd)
                 conn.commit()
-        elif device[1] == 'ASA':
+
+        elif device[1] == 'ASA':  # 如果设备是ASA
+            # 获取设备show run, 注意获取ASA配置的方法不一样
             run_config_raw = ssh_multicmd_asa(str(device[0]), device[4], device[5], ['enable', device[6], 'terminal pager 0', 'more system:running-config'])
             list_run_config = run_config_raw.split('\n')
             location = 0
-            host_location = 0
+            host_location = 0  # 用来找到hostname出现的位置
             for i in list_run_config:
-                if re.match('^hostname .*', i):
+                if re.match('^hostname .*', i):  # 注意匹配hostname的方法不一样,因为配置中会多次出现hostname
                     host_location = location  # 定位hostname所在位置
                 else:
                     location += 1
-            list_run_config = list_run_config[host_location:-4]  # 截取hostname开始往后的部分
+            list_run_config = list_run_config[host_location:-4]  # 截取hostname开始往后的部分, 去除最后一些无用部分
             run_config = '\n'.join(list_run_config)  # 再次还原为字串形式的配置
 
+            # 计算获取配置的MD5值
             m = hashlib.md5()
             m.update(run_config.encode())
             md5_value = m.hexdigest()
 
+            # 获取最近一次保存配置的MD5值,注意添加设备的时候就会做一次备份!
+            # order by date desc limit 1 查询最近一次记录
             cursor.execute("select hash from qytdb_deviceconfig where name = '" + device[2] + "' order by date desc limit 1")
             result = cursor.fetchall()
-            if result[0][0] == md5_value:
+            if result[0][0] == md5_value:  # 如果本次配置的MD5值,与上一次备份配置的MD5值相同!略过此次操作
                 continue
             else:
-                # print('write db')
+                # 如果本次配置的MD5值,与上一次备份配置的MD5值不相同,备份配置与MD5值到数据库
                 sqlcmd = "INSERT INTO qytdb_deviceconfig (name, hash, config, date) VALUES ('" + device[2] + "', '" + md5_value + "', '" + run_config + "', '" + str(datetime.now()) + "')"
                 cursor.execute(sqlcmd)
                 conn.commit()
