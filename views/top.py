043a08dac65525db1f5027651867c632e876fe55
cq_bomb <collinsctk@gmail.com>
2018-12-26 10:52:42 +0800

    注释!

--- views/top.py
+++ views/top.py
@@ -9,9 +9,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+
+# 图片所在目录
 DIR = '/static/images/top/'
 EDGE_LENGTH_MAIN = 150
 
+# 添加节点
 nodes = []
 
 nodes.append({'id': 1, 'label': 'IOS路由器', 'image': DIR + 'router_isr.png', 'shape': 'image'})
@@ -22,9 +25,10 @@ nodes.append({'id': 5, 'label': 'Access_SW2', 'image': DIR + 'nexus9200.png', 's
 nodes.append({'id': 6, 'label': 'WIN7', 'image': DIR + 'desktop_new.png', 'shape': 'image'})
 nodes.append({'id': 7, 'label': '互联网', 'image': DIR + 'internet_ico.png', 'shape': 'image'})
 
+# 添加连线
 edges = []
 
-edges.append({'from': 1, 'to': 2, 'length': EDGE_LENGTH_MAIN, 'label': "IOS:E0/0 --- ASA G1"})
+edges.append({'from': 1, 'to': 2, 'length': EDGE_LENGTH_MAIN, 'label': "IOS:E0/0 --- ASA G1"})  # 可以做注释
 edges.append({'from': 1, 'to': 7, 'length': EDGE_LENGTH_MAIN})
 edges.append({'from': 2, 'to': 3, 'length': EDGE_LENGTH_MAIN})
 edges.append({'from': 3, 'to': 4, 'length': EDGE_LENGTH_MAIN})
@@ -33,10 +37,10 @@ edges.append({'from': 4, 'to': 6, 'length': EDGE_LENGTH_MAIN})
 
 
 @login_required()
-def top(request):
+def top(request):  # 返回拓扑图页面'top_map.html', 拓扑图使用过JS获取JSON数据后绘制的
     return render(request, 'top_map.html')
 
 
 @login_required()
-def top_json(request):
+def top_json(request):  # 提供拓扑图绘制的JSON数据, 返回节点列表和连线列表
     return JsonResponse({'nodes': nodes, 'edges': edges})
